55a182e30632b481cd9e340248a2f431ef814db2
valentine-bread <vavandeat@gmail.com>
2022-11-29 22:25:23 +0300

    F2

--- lab4.py
+++ lab4.py
@@ -22,36 +22,32 @@ cap.set(cv.CAP_PROP_FPS, 30)
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
-@profile
-def foo():
-    while cv.waitKey(1) != 27:    #press Esc
-        start_time, fps, tmp_fps = time.time(), 0, 0
-        tmp_fps += 1    
-        ret, frame = cap.read()
-        frame = cv.resize(frame, None, fx=0.8, fy=0.8, interpolation= cv.INTER_AREA)
-        
-        results = model_yolo(frame)
-        data = list(filter(lambda x: x['cls'] == 45, results.crop(save=False)))
-        if len(data) > 0:
-            batch = torch.zeros([len(data),3,224,224])
-            for i in range(len(data)):
-                batch[i] = transforms(Image.fromarray(data[i]['im']))
-                
-            preds = model(batch.to(device))
-            preds_class = torch.nn.functional.softmax(preds, dim=1)[:,1].data.cpu().numpy() 
+while cv.waitKey(1) != 27:    #press Esc
+    start_time, fps, tmp_fps = time.time(), 0, 0
+    tmp_fps += 1    
+    ret, frame = cap.read()
+    frame = cv.resize(frame, None, fx=0.8, fy=0.8, interpolation= cv.INTER_AREA)
+    
+    results = model_yolo(frame)
+    data = list(filter(lambda x: x['cls'] == 45, results.crop(save=False)))
+    if len(data) > 0:
+        batch = torch.zeros([len(data),3,224,224])
+        for i in range(len(data)):
+            batch[i] = transforms(Image.fromarray(data[i]['im']))
             
-            for temp_frame, pred in zip(data, preds_class):
-                x = temp_frame['box']
-                cv.rectangle(frame, (int(x[0]),int(x[1])), (int(x[2]),int(x[3])), (0,0,255), 2)
-                cv.putText(frame, ('dirty' if pred > 0.5 else 'cleaned') + '  '+ temp_frame['label'],(int(x[0]),int(x[1])), cv.FONT_HERSHEY_SIMPLEX, 1 ,(255,255,255),2,cv.LINE_AA)
+        preds = model(batch.to(device))
+        preds_class = torch.nn.functional.softmax(preds, dim=1)[:,1].data.cpu().numpy() 
         
-        if (time.time() - start_time) > 1:  
-            start_time,fps, tmp_fps = time.time(), tmp_fps, 0  
-        cv.putText(frame, str(fps) ,(20,20), cv.FONT_HERSHEY_SIMPLEX, 1 ,(255,255,255),2,cv.LINE_AA)
-        
-        cv.imshow('Input', frame)
-
-foo()
+        for temp_frame, pred in zip(data, preds_class):
+            x = temp_frame['box']
+            cv.rectangle(frame, (int(x[0]),int(x[1])), (int(x[2]),int(x[3])), (0,0,255), 2)
+            cv.putText(frame, ('dirty' if pred > 0.5 else 'cleaned') + '  '+ temp_frame['label'],(int(x[0]),int(x[1])), cv.FONT_HERSHEY_SIMPLEX, 1 ,(255,255,255),2,cv.LINE_AA)
+    
+    if (time.time() - start_time) > 1:  
+        start_time,fps, tmp_fps = time.time(), tmp_fps, 0  
+    cv.putText(frame, str(fps) ,(20,20), cv.FONT_HERSHEY_SIMPLEX, 1 ,(255,255,255),2,cv.LINE_AA)
+    
+    cv.imshow('Input', frame)
 
 cap.release()
 cv.destroyAllWindows()
